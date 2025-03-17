04eed6325943f8a6e82c64341cb5fcf2bb08b3c8
StayIbrahim <ibrahimh5667@gmail.com>
2025-03-17 10:50:14 +0100

    graphApp

--- indexation/settings.py
+++ indexation/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+NEO4J_URI = "bolt://localhost:7687"
+NEO4J_USERNAME = "neo4j"
+NEO4J_PASSWORD = "12345678"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
